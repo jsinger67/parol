d5fac4c0acfac2f92e30e43f282e6919fe753e6b
Jörg Singer <singer.joerg@gmx.de>
2021-11-18 12:20:09 +0100

    Referencing new version of parol_runtime

--- build_parsers.ps1
+++ build_parsers.ps1
@@ -1,6 +1,53 @@
-cargo run --bin parol -- -f ./src/parser/parol-grammar.par -e ./src/parser/parol-grammar-exp.par -p ./src/parser/parol_parser.rs -a ./src/parser/parol_grammar_trait.rs -t ParolGrammar -m parser::parol_grammar
-cargo run --bin parol -- -f ./examples/calc/calc.par -e ./examples/calc/calc-exp.par -p ./examples/calc/calc_parser.rs -a ./examples/calc/calc_grammar_trait.rs -t CalcGrammar -m calc_grammar
-# cargo run --bin parol -- -f ./examples/json/json.par -e ./examples/json/json-exp.par -p ./examples/json/json_parser.rs -a ./examples/json/json_grammar_trait.rs -t JsonGrammar -m json_grammar
-cargo run --bin parol -- -f ./examples/list/list.par -e ./examples/list/list-exp.par -p ./examples/list/list_parser.rs -a ./examples/list/list_grammar_trait.rs -t ListGrammar -m list_grammar
-cargo run --bin parol -- -f ./examples/oberon_0/oberon_0.par -e ./examples/oberon_0/oberon_0-exp.par -p ./examples/oberon_0/oberon_0_parser.rs -a ./examples/oberon_0/oberon_0_grammar_trait.rs -t Oberon0Grammar -m oberon_0_grammar
-cargo run --bin parol -- -f ./examples/scanner_states/scanner_states.par -e ./examples/scanner_states/scanner_states-exp.par -p ./examples/scanner_states/scanner_states_parser.rs -a ./examples/scanner_states/scanner_states_grammar_trait.rs -t ScannerStatesGrammar -m scanner_states_grammar
+$ErrorCont = 0
+
+Write-Host "Building release. Please wait..." -ForegroundColor Cyan
+cargo build --release
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Building parol..." -ForegroundColor Cyan
+./target/release/parol -f ./src/parser/parol-grammar.par -e ./src/parser/parol-grammar-exp.par -p ./src/parser/parol_parser.rs -a ./src/parser/parol_grammar_trait.rs -t ParolGrammar -m parser::parol_grammar
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Building Calc example..." -ForegroundColor Cyan
+./target/release/parol -f ./examples/calc/calc.par -e ./examples/calc/calc-exp.par -p ./examples/calc/calc_parser.rs -a ./examples/calc/calc_grammar_trait.rs -t CalcGrammar -m calc_grammar
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Building List example..." -ForegroundColor Cyan
+./target/release/parol -f ./examples/list/list.par -e ./examples/list/list-exp.par -p ./examples/list/list_parser.rs -a ./examples/list/list_grammar_trait.rs -t ListGrammar -m list_grammar
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Building Oberon-0 example..." -ForegroundColor Cyan
+./target/release/parol -f ./examples/oberon_0/oberon_0.par -e ./examples/oberon_0/oberon_0-exp.par -p ./examples/oberon_0/oberon_0_parser.rs -a ./examples/oberon_0/oberon_0_grammar_trait.rs -t Oberon0Grammar -m oberon_0_grammar
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Building Scanner States example..." -ForegroundColor Cyan
+./target/release/parol -f ./examples/scanner_states/scanner_states.par -e ./examples/scanner_states/scanner_states-exp.par -p ./examples/scanner_states/scanner_states_parser.rs -a ./examples/scanner_states/scanner_states_grammar_trait.rs -t ScannerStatesGrammar -m scanner_states_grammar
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+# Final message
+# --------------------------------------------------------------------------------------------------
+if ($ErrorCont -gt 0) {
+    $Msg = "$ErrorCount error(s) occurred during builds."
+    Write-Host -Object $Msg  -ForegroundColor Red
+} else {
+    Write-Host "All builds successfully executed." -ForegroundColor Green
+}
--- run_parsers.ps1
+++ run_parsers.ps1
@@ -1,7 +1,61 @@
-cargo run --bin parol -- -f ./src/parser/parol-grammar.par -v
-cargo run --example calc -- ./examples/calc/calc_test.txt
-cargo run --example list -- ./examples/list/list_test.txt
-cargo run --example oberon_0 -- ./examples/oberon_0/Sample.mod
+$ErrorCont = 0
+
+Write-Host "Building release. Please wait..." -ForegroundColor Cyan
+cargo build --release
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+Write-Host "Building examples in release. Please wait..." -ForegroundColor Cyan
+cargo build --examples --release
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Running parol on its own parol-grammar..." -ForegroundColor Cyan
+./target/release/parol -f ./src/parser/parol-grammar.par -v
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Running Calc example..." -ForegroundColor Cyan
+./target/release/examples/calc ./examples/calc/calc_test.txt
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Running List example..." -ForegroundColor Cyan
+./target/release/examples/list ./examples/list/list_test.txt
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Running Oberon-0 example..." -ForegroundColor Cyan
+./target/release/examples/oberon_0 ./examples/oberon_0/Sample.mod
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+Write-Host "Running Scanner States example..." -ForegroundColor Cyan
+./target/release/examples/scanner_states ./examples/scanner_states/scanner_states_test.txt
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
 
 # Some of the example grammars will fail because they don't pass the basic grammar checks.
-# Get-ChildItem .\data\*.par | ForEach-Object { cargo run --bin parol -- -f $_.FullName }
+# Get-ChildItem ./data/*.par | ForEach-Object { ./target/release/parol.exe parol -f $_.FullName }
+
+# --------------------------------------------------------------------------------------------------
+# Final message
+# --------------------------------------------------------------------------------------------------
+if ($ErrorCont -gt 0) {
+    $Msg = "$ErrorCount error(s) occurred."
+    Write-Host -Object $Msg  -ForegroundColor Red
+} else {
+    Write-Host "All examples successfully executed." -ForegroundColor Green
+}
