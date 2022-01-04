cda926ac6d442979810f862263964b1282d8b98c
Techcable <Techcable@techcable.net>
2022-01-04 02:29:17 -0700

    Take adavantage of the new build scripts API
    Even when the original is removed, this produces output that is 100%
    compatible with the version generated by the original CLI.
    
    And also it *automatically* detects grammar changes :)
    
    Requires jsinger67/parol#6

---
+++ build.rs
@@ -0,0 +1,18 @@
+use parol::build::Builder;
+
+fn main() {
+    // CLI equivalent
+    // parol -f ./json.par -e ./json-exp.par -p ./src/json_parser.rs -a ./src/json_grammar_trait.rs -t JsonGrammar -m json_grammar
+    
+    // TODO: Add feature-flag to use `Builder::for_build_script`
+
+    Builder::with_explicit_output_dir("src")
+        .grammar_file("json.par")
+        .expanded_grammar_output_file("../json-exp.par")
+        .parser_output_file("json_parser.rs")
+        .actions_output_file("json_grammar_trait.rs")
+        .user_type_name("JsonGrammar")
+        .user_trait_module_name("json_grammar")
+        .generate_parser()
+        .unwrap();
+}
