0540500af1a89ecfe25fb72ef928603fa851b16d
Ryo Hirayama <ryo12redstone@gmail.com>
2022-12-05 00:52:14 +0900

    Use \s for WHITESPACE_TOKEN

--- crates/parol_runtime/src/lexer/tokenizer.rs
+++ crates/parol_runtime/src/lexer/tokenizer.rs
@@ -20,7 +20,7 @@ pub const NEW_LINE_TOKEN: &str = r###"\r\n|\r|\n"###;
 ///
 /// Regular expression for any whitespace
 ///
-pub const WHITESPACE_TOKEN: &str = r###"[ \t]+"###;
+pub const WHITESPACE_TOKEN: &str = r###"\s+"###;
 
 ///
 /// Regular expression that matches any other token. With this you can detect
