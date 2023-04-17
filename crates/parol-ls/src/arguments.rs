74c7a3adc47aafb8fdaee3b4251d972b4069a1e5
Aumy <aumy@outlook.jp>
2023-04-18 00:21:30 +0900

    Add option --stdio

--- crates/parol-ls/src/arguments.rs
+++ crates/parol-ls/src/arguments.rs
@@ -7,11 +7,19 @@ use clap::Parser;
 #[clap(author, version, about)]
 pub struct Arguments {
     /// Server's IP address
-    #[clap(short = 'a', long = "address", default_value = "127.0.0.1")]
+    #[clap(
+        short = 'a',
+        long = "address",
+        group = "tcp",
+        default_value = "127.0.0.1"
+    )]
     pub ip_address: IpAddr,
     /// Server's port
-    #[clap(short = 's', long = "socket", default_value = "7061")]
+    #[clap(short = 's', long = "socket", group = "tcp", default_value = "7061")]
     pub port_number: u16,
+    /// Use stdio
+    #[clap(long = "stdio", conflicts_with = "tcp")]
+    pub stdio: bool,
     /// Lookahead limit
     #[clap(short = 'k', long = "lookahead", default_value = "3")]
     pub lookahead: usize,
