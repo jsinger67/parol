jsinger67/parol

commit 4dd1172c602055cb909246aa6b9883287e3b20d6

follow_k now uses cached results from step k-1

crates/parol/src/analysis/follow.rs
@@ -33,7 +33,7 @@ pub(crate) type ResultMap = HashMap<Pos, DomainType>;
 /// The type of the function in the equation system
 /// It is called for each non-terminal
 type TransferFunction =
-    Arc<dyn Fn(Arc<ResultMap>, Arc<RwLock<Vec<DomainType>>>) -> DomainType + Send + Sync + 'static>;
+    Arc<dyn Fn(Arc<ResultMap>, Arc<RwLock<FollowSet>>) -> DomainType + Send + Sync + 'static>;
 
 type EquationSystem = HashMap<Pos, TransferFunction>;
 
@@ -44,18 +44,33 @@ type EquationSystem = HashMap<Pos, TransferFunction>;
 /// and the non-terminal vector) don't change anymore.
 ///
 /// ## Parameters
-///   * `Arc<EquationSystem>` - An `Arc` of an `EquationSystem` struct.
-///   * `Arc<ResultMap>` - An `Arc` of a `ResultMap` struct.
-///   * `Arc<HashMap<Pos, usize>>` - An `Arc` of a `HashMap` of `Pos` and `usize`.
-///   * `Arc<RwLock<Vec<DomainType>>>` - An `Arc` of a `RwLock` of a `Vec` of `DomainType`.
+///   * `es: Arc<EquationSystem>` - An `Arc` of an `EquationSystem` struct.
+///     Each function in this equation system is called in each iteration step while iterating over
+///     the non-terminal positions of the grammar which are taken from the second parameter of this
+///     function. Each position is used to retrieve the associated function from this equation
+///     system.
+///     It is wrapped in an `Arc` to make it read accessible from multiple threads.
+///   * `result_map: Arc<ResultMap>` - An `Arc` of a `ResultMap` struct.
+///     This is the actual input for each iteration generated by the previous iteration.
+///     It is wrapped in an `Arc` to make it read accessible from multiple threads.
+///   * `non_terminal_positions: Arc<HashMap<Pos, usize>>` - An `Arc` of a `HashMap` of `Pos` and `usize`.
+///     This is the association of non-terminal positions to non-terminal indices in the
+///     non-terminal vector (the fourth parameter of this function) and is used to find the correct
+///     place where the non-terminal result has to be accumulated.
+///     It is wrapped in an `Arc` to make it read accessible from multiple threads.
+///   * `non_terminal_results: Arc<RwLock<FollowSet>>` - An `Arc` of a `RwLock` of a `Vec` of `DomainType`.
+///     This is the actual value returned by the [follow_k] function and is amended in each
+///     iteration step by combining all results for all position of a certain non-terminal into a
+///     single result (a k-tuple, i.e. a trie of terminal strings).
+///     It is wrapped in an `Arc<RwLock>` to make it write accessible from multiple threads.
 /// ## Return Value
-/// The `StepFunction` returns a `ResultMap` struct.
+/// The `StepFunction` returns a `ResultMap` struct that was extended in each iteration step.
 type StepFunction = Arc<
     dyn Fn(
         Arc<EquationSystem>,
         Arc<ResultMap>,
         Arc<HashMap<Pos, usize>>,
-        Arc<RwLock<Vec<DomainType>>>,
+        Arc<RwLock<FollowSet>>,
     ) -> ResultMap,
 >;
 
@@ -83,19 +98,6 @@ pub fn follow_k(
     let first_k_of_nt = Arc::new(first_cache.get(k, grammar_config).1);
 
     let start_symbol = cfg.get_start_symbol();
-
-    let non_terminal_results: Arc<RwLock<Vec<DomainType>>> = Arc::new(RwLock::new(
-        cfg.get_non_terminal_set()
-            .iter()
-            .fold(Vec::new(), |mut acc, nt| {
-                if nt == start_symbol {
-                    acc.push(DomainType::end(k));
-                } else {
-                    acc.push(DomainType::new(k));
-                }
-                acc
-            }),
-    ));
 
     let non_terminal_index_finder = cfg.get_non_terminal_index_function();
 
@@ -137,7 +139,7 @@ pub fn follow_k(
     //     move |es: Arc<EquationSystem>,
     //           result_map: Arc<ResultMap>,
     //           non_terminal_positions: Arc<HashMap<Pos, usize>>,
-    //           non_terminal_results: Arc<RwLock<Vec<DomainType>>>| {
+    //           non_terminal_results: Arc<RwLock<FollowSet>>| {
     //         let mut new_result_vector = ResultMap::new();
     //         for (pos, _) in result_map.iter() {
     //             // Call each function of the equation system and put the
@@ -163,7 +165,7 @@ pub fn follow_k(
         move |es: Arc<EquationSystem>,
               result_map: Arc<ResultMap>,
               non_terminal_positions: Arc<HashMap<Pos, usize>>,
-              non_terminal_results: Arc<RwLock<Vec<DomainType>>>| {
+              non_terminal_results: Arc<RwLock<FollowSet>>| {
             let (tx, rx) = channel();
             let iter = &mut result_map.iter().map(|(pos, _)| *pos) as &mut dyn Iterator<Item = Pos>;
             let mut new_result_vector = ResultMap::new();
@@ -202,6 +204,22 @@ pub fn follow_k(
             new_result_vector
         },
     );
+
+    let non_terminal_results: Arc<RwLock<FollowSet>> = Arc::new(RwLock::new(if k <= 1 {
+        cfg.get_non_terminal_set()
+            .iter()
+            .fold(Vec::new(), |mut acc, nt| {
+                if nt == start_symbol {
+                    acc.push(DomainType::end(k));
+                } else {
+                    acc.push(DomainType::new(k));
+                }
+                acc
+            })
+    } else {
+        let CacheEntry(_, f) = follow_cache.get(k - 1, grammar_config, first_cache);
+        f
+    }));
 
     let mut result_map = Arc::new(if k <= 1 {
         non_terminal_positions
@@ -252,7 +270,7 @@ fn update_production_equations<T>(
     prod_num: usize,
     pr: &Pr,
     non_terminal_index_finder: &impl Fn(&str) -> usize,
-    first_k_of_nt: Arc<Vec<DomainType>>,
+    first_k_of_nt: Arc<FollowSet>,
     terminal_index: Arc<T>,
     k: usize,
 ) -> EquationSystem
@@ -338,7 +356,7 @@ where
             es.insert(
                 (prod_num, *symbol_index).into(),
                 Arc::new(
-                    move |result_map, non_terminal_results: Arc<RwLock<Vec<DomainType>>>| {
+                    move |result_map, non_terminal_results: Arc<RwLock<FollowSet>>| {
                         result_function(result_map, non_terminal_results.clone())
                             .k_concat(non_terminal_results.read().unwrap().get(nt).unwrap(), k)
                     },

examples/basic_interpreter/src/basic_parser.rs
@@ -248,10 +248,10 @@ pub const LOOKAHEAD_AUTOMATA: &[LookaheadDFA; 58] = &[
     LookaheadDFA {
         prod0: -1,
         transitions: &[
-            Trans(0, 0, 3, 2),
-            Trans(0, 9, 1, -1),
-            Trans(1, 0, 3, 2),
-            Trans(1, 6, 2, 1),
+            Trans(0, 0, 2, 2),
+            Trans(0, 9, 2, 2),
+            Trans(2, 0, 2, 2),
+            Trans(2, 6, 1, 1),
         ],
         k: 2,
     },

examples/oberon2/src/oberon2_parser.rs
@@ -940,9 +940,9 @@ pub const LOOKAHEAD_AUTOMATA: &[LookaheadDFA; 130] = &[
     LookaheadDFA {
         prod0: -1,
         transitions: &[
-            Trans(0, 68, 1, -1),
-            Trans(1, 18, 3, 23),
-            Trans(1, 21, 3, 23),
+            Trans(0, 68, 1, 23),
+            Trans(1, 18, 1, 23),
+            Trans(1, 21, 1, 23),
             Trans(1, 22, 2, 22),
         ],
         k: 2,
@@ -951,9 +951,9 @@ pub const LOOKAHEAD_AUTOMATA: &[LookaheadDFA; 130] = &[
     LookaheadDFA {
         prod0: -1,
         transitions: &[
-            Trans(0, 68, 1, -1),
-            Trans(1, 18, 3, 21),
-            Trans(1, 21, 3, 21),
+            Trans(0, 68, 1, 21),
+            Trans(1, 18, 1, 21),
+            Trans(1, 21, 1, 21),
             Trans(1, 22, 2, 20),
         ],
         k: 2,

commit 7df8ff7f662d85cc56386a5b584e86cdb8581b87

Fixed problem in calculation of follow(k) sets introduced in 4dd1172 Some minor tweaks

crates/parol/src/analysis/first.rs
@@ -8,9 +8,9 @@ use crate::analysis::FirstCache;
 use crate::grammar::symbol_string::SymbolString;
 use crate::{CompiledTerminal, GrammarConfig, KTuple, KTuples, Pr, Symbol, TerminalKind};
 use parol_runtime::log::trace;
-use std::sync::mpsc::channel;
+// use std::sync::mpsc::channel;
 use std::sync::Arc;
-use std::thread;
+// use std::thread;
 
 /// 0: KTuples for terminals in terminal-index order
 /// 1: KTuples for non-terminals in non-terminal-index (alphabetical) order
@@ -84,55 +84,55 @@ pub fn first_k(grammar_config: &GrammarConfig, k: usize, first_cache: &FirstCach
     let equation_system = Arc::new(equation_system);
 
     // Single threaded variant
-    // let step_function: StepFunction = {
-    //     Arc::new(
-    //         move |es: Arc<EquationSystem>, result_vector: Arc<ResultVector>| {
-    //             let mut new_result_vector: ResultVector =
-    //                 vec![DomainType::new(k); result_vector.len()];
-    //             for pr_i in 0..pr_count {
-    //                 let r = es[pr_i](result_vector.clone());
-    //                 new_result_vector[pr_i] = r.clone();
-    //                 new_result_vector[pr_count + nt_for_production[pr_i]].append(r);
-    //             }
-    //             new_result_vector
-    //         },
-    //     )
-    // };
-
-    // Heuristically tweaked
-    let factor = 1;
-    let max_threads: usize = num_cpus::get() * factor;
-
     let step_function: StepFunction = {
         Arc::new(
             move |es: Arc<EquationSystem>, result_vector: Arc<ResultVector>| {
-                let (tx, rx) = channel();
-                let iter = &mut (0..pr_count) as &mut dyn Iterator<Item = usize>;
-                let mut new_result_vector = vec![DomainType::new(k); result_vector.len()];
-                loop {
-                    let mut threads = 0;
-                    iter.take(max_threads).for_each(|pr_i| {
-                        threads += 1;
-                        let tx = tx.clone();
-                        let es = es.clone();
-                        let result_vector = result_vector.clone();
-                        thread::spawn(move || {
-                            tx.send((pr_i, es[pr_i](result_vector))).unwrap();
-                        });
-                    });
-                    (0..threads).for_each(|_| {
-                        let (pr_i, r) = rx.recv().unwrap();
-                        new_result_vector[pr_i] = r.clone();
-                        new_result_vector[pr_count + nt_for_production[pr_i]].append(r);
-                    });
-                    if threads == 0 {
-                        break;
-                    }
+                let mut new_result_vector: ResultVector =
+                    vec![DomainType::new(k); result_vector.len()];
+                for pr_i in 0..pr_count {
+                    let r = es[pr_i](result_vector.clone());
+                    new_result_vector[pr_count + nt_for_production[pr_i]].append(&r);
+                    new_result_vector[pr_i] = r;
                 }
                 new_result_vector
             },
         )
     };
+
+    // Heuristically tweaked
+    // let factor = 1;
+    // let max_threads: usize = num_cpus::get() * factor;
+
+    // let step_function: StepFunction = {
+    //     Arc::new(
+    //         move |es: Arc<EquationSystem>, result_vector: Arc<ResultVector>| {
+    //             let (tx, rx) = channel();
+    //             let iter = &mut (0..pr_count) as &mut dyn Iterator<Item = usize>;
+    //             let mut new_result_vector = vec![DomainType::new(k); result_vector.len()];
+    //             loop {
+    //                 let mut threads = 0;
+    //                 iter.take(max_threads).for_each(|pr_i| {
+    //                     threads += 1;
+    //                     let tx = tx.clone();
+    //                     let es = es.clone();
+    //                     let result_vector = result_vector.clone();
+    //                     thread::spawn(move || {
+    //                         tx.send((pr_i, es[pr_i](result_vector))).unwrap();
+    //                     });
+    //                 });
+    //                 (0..threads).for_each(|_| {
+    //                     let (pr_i, r) = rx.recv().unwrap();
+    //                     new_result_vector[pr_i] = r.clone();
+    //                     new_result_vector[pr_count + nt_for_production[pr_i]].append(r);
+    //                 });
+    //                 if threads == 0 {
+    //                     break;
+    //                 }
+    //             }
+    //             new_result_vector
+    //         },
+    //     )
+    // };
 
     let mut result_vector = Arc::new(if k <= 1 {
         (0..pr_count + nt_count).fold(Vec::with_capacity(pr_count + nt_count), |mut acc, i| {

crates/parol/src/analysis/follow.rs
@@ -12,9 +12,9 @@ use crate::{GrammarConfig, KTuple, KTuples, Pos, Pr, Symbol, TerminalKind};
 use parol_runtime::lexer::FIRST_USER_TOKEN;
 use parol_runtime::log::trace;
 use std::collections::HashMap;
-use std::sync::mpsc::channel;
+// use std::sync::mpsc::channel;
 use std::sync::{Arc, RwLock};
-use std::thread;
+// use std::thread;
 
 use super::FollowCache;
 
@@ -135,75 +135,76 @@ pub fn follow_k(
     let equation_system = Arc::new(equation_system);
 
     // Single threaded variant
-    // let step_function: StepFunction = Arc::new(
-    //     move |es: Arc<EquationSystem>,
-    //           result_map: Arc<ResultMap>,
-    //           non_terminal_positions: Arc<HashMap<Pos, usize>>,
-    //           non_terminal_results: Arc<RwLock<FollowSet>>| {
-    //         let mut new_result_vector = ResultMap::new();
-    //         for (pos, _) in result_map.iter() {
-    //             // Call each function of the equation system and put the
-    //             // result into the new result vector.
-    //             let pos_result = es[pos](result_map.clone(), non_terminal_results.clone());
-    //             new_result_vector.insert(*pos, pos_result.clone());
-
-    //             // Also combine the result to the non_terminal_results.
-    //             let sym = non_terminal_positions.get(pos).unwrap();
-    //             if let Some(set) = non_terminal_results.write().unwrap().get_mut(*sym) {
-    //                 *set = set.union(pos_result).0;
-    //             }
-    //         }
-    //         new_result_vector
-    //     },
-    // );
-
-    // Heuristically tweaked
-    let factor = 4;
-    let max_threads: usize = num_cpus::get() * factor;
-
     let step_function: StepFunction = Arc::new(
         move |es: Arc<EquationSystem>,
               result_map: Arc<ResultMap>,
               non_terminal_positions: Arc<HashMap<Pos, usize>>,
               non_terminal_results: Arc<RwLock<FollowSet>>| {
-            let (tx, rx) = channel();
-            let iter = &mut result_map.iter().map(|(pos, _)| *pos) as &mut dyn Iterator<Item = Pos>;
             let mut new_result_vector = ResultMap::new();
-            loop {
-                let mut threads = 0;
-                iter.take(max_threads).for_each(|pos| {
-                    threads += 1;
-                    let tx = tx.clone();
-                    let es = es.clone();
-                    let result_map = result_map.clone();
-                    let non_terminal_results = non_terminal_results.clone();
+            for (pos, _) in result_map.iter() {
+                // Call each function of the equation system
+                let pos_result = es[pos](result_map.clone(), non_terminal_results.clone());
 
-                    // Call each function of the equation system...
-                    thread::spawn(move || {
-                        tx.send((pos, es[&pos](result_map, non_terminal_results)))
-                            .unwrap();
-                    });
-                });
-
-                (0..threads).for_each(|_| {
-                    let (pos, pos_result) = rx.recv().unwrap();
-
-                    // ...and put the result into the new result vector.
-                    new_result_vector.insert(pos, pos_result.clone());
-
-                    // Also combine the result to the non_terminal_results.
-                    let sym = non_terminal_positions.get(&pos).unwrap();
-                    if let Some(set) = non_terminal_results.write().unwrap().get_mut(*sym) {
-                        *set = set.union(pos_result).0;
-                    }
-                });
-                if threads == 0 {
-                    break;
+                // Combine the result to the non_terminal_results.
+                let sym = non_terminal_positions.get(pos).unwrap();
+                if let Some(set) = non_terminal_results.write().unwrap().get_mut(*sym) {
+                    *set = set.union(&pos_result).0;
                 }
+
+                // And put the result into the new result vector.
+                new_result_vector.insert(*pos, pos_result);
             }
             new_result_vector
         },
     );
+
+    // Heuristically tweaked
+    // let factor = 4;
+    // let max_threads: usize = num_cpus::get() * factor;
+
+    // let step_function: StepFunction = Arc::new(
+    //     move |es: Arc<EquationSystem>,
+    //           result_map: Arc<ResultMap>,
+    //           non_terminal_positions: Arc<HashMap<Pos, usize>>,
+    //           non_terminal_results: Arc<RwLock<FollowSet>>| {
+    //         let (tx, rx) = channel();
+    //         let iter = &mut result_map.iter().map(|(pos, _)| *pos) as &mut dyn Iterator<Item = Pos>;
+    //         let mut new_result_vector = ResultMap::new();
+    //         loop {
+    //             let mut threads = 0;
+    //             iter.take(max_threads).for_each(|pos| {
+    //                 threads += 1;
+    //                 let tx = tx.clone();
+    //                 let es = es.clone();
+    //                 let result_map = result_map.clone();
+    //                 let non_terminal_results = non_terminal_results.clone();
+
+    //                 // Call each function of the equation system...
+    //                 thread::spawn(move || {
+    //                     tx.send((pos, es[&pos](result_map, non_terminal_results)))
+    //                         .unwrap();
+    //                 });
+    //             });
+
+    //             (0..threads).for_each(|_| {
+    //                 let (pos, pos_result) = rx.recv().unwrap();
+
+    //                 // ...and put the result into the new result vector.
+    //                 new_result_vector.insert(pos, pos_result.clone());
+
+    //                 // Also combine the result to the non_terminal_results.
+    //                 let sym = non_terminal_positions.get(&pos).unwrap();
+    //                 if let Some(set) = non_terminal_results.write().unwrap().get_mut(*sym) {
+    //                     *set = set.union(pos_result).0;
+    //                 }
+    //             });
+    //             if threads == 0 {
+    //                 break;
+    //             }
+    //         }
+    //         new_result_vector
+    //     },
+    // );
 
     let non_terminal_results = Arc::new(RwLock::new(cfg.get_non_terminal_set().iter().fold(
         Vec::new(),

crates/parol/src/analysis/k_tuple.rs
@@ -227,11 +227,13 @@ impl Terminals {
     }
 
     /// Checks if self is an Epsilon
+    #[inline]
     pub fn is_eps(&self) -> bool {
         self.i == 1 && self.t[0].is_eps()
     }
 
     /// Checks if self is an end-of-input symbol
+    #[inline]
     pub fn is_end(&self) -> bool {
         self.i == 1 && self.t[0].is_end()
     }
@@ -468,7 +470,7 @@ impl KTuple {
     }
 
     ///
-    /// Creates a new object from a slice of other objects
+    /// Creates a new object from a slice of CompiledTerminals
     ///
     pub fn from_slice(others: &[CompiledTerminal], k: usize) -> Self {
         let terminals = Terminals::from_slice(others, k);

crates/parol/src/analysis/k_tuples.rs
@@ -42,14 +42,14 @@ impl KTuples {
     }
 
     /// Appends another KTuples item to self
-    pub fn append(&mut self, other: Self) -> bool {
+    pub fn append(&mut self, other: &Self) -> bool {
         let count = self.0.len();
         self.0.append(&other.0);
         count != self.0.len()
     }
 
     /// Creates a union with another KTuples and self
-    pub fn union(&self, other: Self) -> (Self, bool) {
+    pub fn union(&self, other: &Self) -> (Self, bool) {
         let (unn, changed) = self.0.union(&other.0);
         let mut tuples = Self(unn, self.1, false);
         tuples.update_completeness();

crates/parol/src/analysis/terminals_trie.rs
@@ -291,7 +291,7 @@ pub(crate) struct TerminalsIter<'a> {
 impl<'a> TerminalsIter<'a> {
     pub(crate) fn new(t: &'a Trie) -> Self {
         let mut this = Self {
-            v: Vec::with_capacity(MAX_K), // Depth of Tie can't exceed MAX_K
+            v: Vec::with_capacity(MAX_K), // Depth of Trie can't exceed MAX_K
         };
         if !t.is_empty() {
             let flags = if t.root[0].e {