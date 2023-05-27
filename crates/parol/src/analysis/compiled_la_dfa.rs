23c27a03d2ba6a7c10882684826e958c3426b16e
Jörg Singer <singer.joerg@gmx.de>
2023-05-27 10:20:53 +0200

    Preparation for optimization using adjacency list

--- crates/parol/src/analysis/compiled_la_dfa.rs
+++ crates/parol/src/analysis/compiled_la_dfa.rs
@@ -3,12 +3,77 @@ use std::collections::BTreeMap;
 use super::lookahead_dfa::{CompiledProductionIndex, StateIndex, INVALID_PROD};
 use super::LookaheadDFA;
 
+// use crate::group_by;
+
 pub(crate) type TerminalIndex = usize;
 
+mod adjacency_list {
+    use std::collections::{HashMap, HashSet};
+    use crate::analysis::lookahead_dfa::CompiledProductionIndex;
+    use crate::analysis::compiled_la_dfa::TerminalIndex;
+
+    use super::CompiledDFA;
+
+    type NodeId = usize;
+
+    #[derive(Debug, Eq, PartialEq, Hash)]
+    struct AdjacentNode {
+        id: usize,
+        term: TerminalIndex,
+    }
+
+    #[derive(Debug)]
+    struct AdjacencyListEntry {
+        neighbors: HashSet<AdjacentNode>,
+    }
+
+    impl AdjacencyListEntry {
+        fn new() -> Self {
+            AdjacencyListEntry {
+                neighbors: HashSet::new(),
+            }
+        }
+
+        fn add_neighbor(&mut self, neighbor: AdjacentNode) {
+            self.neighbors.insert(neighbor);
+        }
+    }
+
+    #[derive(Debug)]
+    struct AdjacencyList {
+        list: HashMap<NodeId, AdjacencyListEntry>,
+        productions: HashMap<NodeId, CompiledProductionIndex>,
+    }
+
+    impl From<&CompiledDFA> for AdjacencyList {
+        fn from(value: &CompiledDFA) -> Self {
+            let mut list = HashMap::new();
+            let mut productions = HashMap::new();
+            list.insert(0, AdjacencyListEntry::new());
+            productions.insert(0, value.prod0);
+
+            for t in &value.transitions {
+                // We use the to-state and the resulting production number
+                list.insert(t.2, AdjacencyListEntry::new());
+                productions.insert(t.2, t.3);
+            }
+
+            for t in &value.transitions {
+                // We add the neighbors (to-state) to each node (from-state)
+                if let Some(f) = list.get_mut(&t.0) {
+                    f.add_neighbor(AdjacentNode { id: t.2, term: t.1 });
+                }
+            }
+
+            AdjacencyList { list, productions }
+        }
+    }
+}
+
 /// A Four-Tuple type consisting of
-/// * Start state index
+/// * Start state index (from-state)
 /// * Terminal index occurred in start state that triggers the transition
-/// * Result state index
+/// * Result state index (to-state)
 /// * Possible ProductionIndex
 type CompiledTransition = (
     StateIndex,
@@ -87,15 +152,22 @@ impl CompiledDFA {
             _combined_states: Vec<usize>,
             _transitions: &[CompiledTransition],
         ) -> (Vec<usize>, Vec<CompiledTransition>) {
-            // 
+            //
             (vec![], vec![])
         }
 
-        // let mut combined_states = transitions.iter().filter(|t| t.3 != INVALID_PROD).gr;
+        let _transitions_to_final_states: Vec<(usize, usize, usize, i32)> = transitions
+            .iter()
+            .filter(|t| t.3 != INVALID_PROD)
+            .cloned()
+            .collect();
+
+        // let groups_of_combinable_states =
+        //     group_by(&combined_states, ||);
+
         // let (new_combined_states, mut transitions) = minimize_step(combined_states, &transitions);
         // combined_states = new_combined_states;
         // while !combined_states.is_empty() {}
-
 
         transitions.sort_by_key(|s| (s.0, s.1));
 
@@ -116,41 +188,41 @@ impl CompiledDFA {
             k,
         } = self;
 
-        fn remove_duplicate_at_index(
-            kept_index: usize,
-            index_to_remove: usize,
-            transitions: &mut [CompiledTransition],
-        ) {
-            // debug_assert!(kept_index < index_to_remove);
-            transitions.iter_mut().for_each(|t| {
-                match t.0.cmp(&index_to_remove) {
-                    std::cmp::Ordering::Less => (),
-                    std::cmp::Ordering::Equal => t.0 = kept_index,
-                    std::cmp::Ordering::Greater => t.0 -= 1,
-                }
-                match t.2.cmp(&index_to_remove) {
-                    std::cmp::Ordering::Less => (),
-                    std::cmp::Ordering::Equal => t.2 = kept_index,
-                    std::cmp::Ordering::Greater => t.2 -= 1,
-                }
-            });
-        }
+        // fn remove_duplicate_at_index(
+        //     kept_index: usize,
+        //     index_to_remove: usize,
+        //     transitions: &mut [CompiledTransition],
+        // ) {
+        //     // debug_assert!(kept_index < index_to_remove);
+        //     transitions.iter_mut().for_each(|t| {
+        //         match t.0.cmp(&index_to_remove) {
+        //             std::cmp::Ordering::Less => (),
+        //             std::cmp::Ordering::Equal => t.0 = kept_index,
+        //             std::cmp::Ordering::Greater => t.0 -= 1,
+        //         }
+        //         match t.2.cmp(&index_to_remove) {
+        //             std::cmp::Ordering::Less => (),
+        //             std::cmp::Ordering::Equal => t.2 = kept_index,
+        //             std::cmp::Ordering::Greater => t.2 -= 1,
+        //         }
+        //     });
+        // }
 
-        let mut changed = true;
-        'NEXT: while changed {
-            changed = false;
-            for (index1, t1) in transitions.iter().enumerate() {
-                for t2 in transitions.iter().skip(index1 + 1) {
-                    // Check for same result production number
-                    // Note that only accepting states carry a valid production number
-                    if t1.3 != INVALID_PROD && t1.3 == t2.3 && t1.2 != t2.2 {
-                        remove_duplicate_at_index(t1.2, t2.2, &mut transitions);
-                        changed = true;
-                        continue 'NEXT;
-                    }
-                }
-            }
-        }
+        // let mut changed = true;
+        // 'NEXT: while changed {
+        //     changed = false;
+        //     for (index1, t1) in transitions.iter().enumerate() {
+        //         for t2 in transitions.iter().skip(index1 + 1) {
+        //             // Check for same result production number
+        //             // Note that only accepting states carry a valid production number
+        //             if t1.3 != INVALID_PROD && t1.3 == t2.3 && t1.2 != t2.2 {
+        //                 remove_duplicate_at_index(t1.2, t2.2, &mut transitions);
+        //                 changed = true;
+        //                 continue 'NEXT;
+        //             }
+        //         }
+        //     }
+        // }
 
         transitions.sort_by_key(|s| (s.0, s.1));
 
