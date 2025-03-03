7f7d771006f3659f26c82cc1f758c076e7056d4e
Jörg Singer <singer.joerg@gmx.de>
2025-03-03 17:39:19 +0100

    fix: parol (par) file generation when `%t_type` is used

--- crates/parol/src/grammar/symbol.rs
+++ crates/parol/src/grammar/symbol.rs
@@ -409,7 +409,7 @@ impl Symbol {
                         } else {
                             user_type.to_string()
                         };
-                    if alias != *n {
+                    if alias != *n && alias != "%t_type" {
                         // Don't print user type if it is the globally defined type
                         write!(s, " : {}", alias).map_err(|e| anyhow!("IO error!: {}", e))?;
                     }
